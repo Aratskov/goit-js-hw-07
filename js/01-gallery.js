73814e89f1c4b75f613c50aef070924c88e04140
Aratskov <ifromukraine2022@gmail.com>
2023-02-19 08:56:03 +0200

    add style 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,17 +3,16 @@ import { galleryItems } from "./gallery-items.js";
 
 // console.log(galleryItems);
 
-
 const gallery = document.querySelector(".gallery");
 const galleryPreview = renderImages(galleryItems);
-gallery.insertAdjacentHTML('beforeend', galleryPreview);
+gallery.insertAdjacentHTML("beforeend", galleryPreview);
 
-
-gallery.addEventListener('click',onOpenModal);
+gallery.addEventListener("click", onOpenModal);
 
 function renderImages(gallery) {
-    return gallery.map(({ preview, original, description }) => {
-        return `
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
         <div class="gallery__item">
         <a class="gallery__link" href="${original}">
         <img
@@ -25,37 +24,38 @@ function renderImages(gallery) {
         </a>
         </div>
         `;
-    }).join('');
+    })
+    .join("");
 }
 
-function onOpenModal(event){
-    
-    window.addEventListener('keydown', modalCloseEsc)
-//     const isGalleryElement = event.target.classList.contains("gallery__image");
-// //   if (!isGalleryElement) {
-// //     return;
-// //   }
-  event.preventDefault()
-    const originalImage = event.target.dataset.source;
-    const instance = basicLightbox.create(`
+function onOpenModal(event) {
+  window.addEventListener("keydown", modalCloseEsc);
+  event.preventDefault();
+
+  const isGalleryElement = event.target.classList.contains("gallery__image");
+  if (!isGalleryElement) {
+    return;
+  }
+
+  const originalImage = event.target.dataset.source;
+  const instance = basicLightbox.create(`
     <img src="${originalImage}" width="800" height="600">
-`)
+`);
 
-instance.show();
+  instance.show();
 }
 
-function modalClose(event){
-window.removeEventListener('keydown', modalCloseEsc);
-
+function modalClose(event) {
+  window.removeEventListener("keydown", modalCloseEsc);
 }
 
-function modalCloseEsc(event){
-    // modalClose()
-    console.log(event.code)
-    if(event.code === "Escape"){
-        modalClose();
-        instance.close( );
-        // console.log(event.code)
-    }
-    // return
+function modalCloseEsc(event) {
+  // modalClose()
+  console.log(event.code);
+  if (event.code === "Escape") {
+    modalClose();
+    instance.close();
+    // console.log(event.code)
+  }
+  // return
 }
