5824a7a23c89f3ab2d91d55748399fb31995eebd
Aratskov <ifromukraine2022@gmail.com>
2023-02-19 08:45:05 +0200

    Add style for 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 const galleryPreview = renderImages(galleryItems);
-gallery.insertAdjacentHTML('beforeend', galleryPreview);
+gallery.insertAdjacentHTML("beforeend", galleryPreview);
 
 function renderImages(gallery) {
-    return gallery.map(({ preview, original, description }) => {
-        return `
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
         <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
         `;
-    }).join('');
+    })
+    .join("");
 }
 
-const lightbox = new SimpleLightbox(".gallery__item", { captionsData: "alt", captionDelay:250});
-
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
